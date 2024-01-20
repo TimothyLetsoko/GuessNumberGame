5a69639342acabd9a584807bf3abf4e62d9e38bc
Timothy Letsoko <timothyletsoko3@gmail.com>
2024-01-20 10:47:31 +0200

    Added a responsive reset button. Only enabled when the score is above 0.

--- src/HomeController.java
+++ src/HomeController.java
@@ -1,2 +1,79 @@
-package PACKAGE_NAME;public class HomeController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+//import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+    @FXML
+    public Button btnPlay;
+    @FXML
+    private Label lblStatus;
+    @FXML
+    private Label lblScore;
+    @FXML
+    private Button btnReset;
+    @FXML
+    private TextField tfGuessedNumber;
+    int mysteryNumber;
+    int score;
+    void randomizeMysteryNumber(){
+        Random random = new Random(System.currentTimeMillis());
+        mysteryNumber = random.nextInt(20);
+    }
+
+    @FXML
+    void play() {
+        if(btnPlay.getText().equals("Guess")){
+            try{
+                int guessedNumber = Integer.parseInt(tfGuessedNumber.getText());
+                if(mysteryNumber == guessedNumber){
+                    lblStatus.setText("You Won!!!");
+                    lblStatus.setTextFill(Color.GREEN);
+                    btnPlay.setText("Play again");
+                    lblScore.setText("Score: "+(++score));
+                    btnReset.setDisable(false);
+                }else if(Math.abs(mysteryNumber-guessedNumber)<5){
+                    lblStatus.setText("You are close");
+                    lblStatus.setTextFill(Color.ORANGE);
+                }else if((guessedNumber-mysteryNumber)>=5){
+                    lblStatus.setText("Too high!");
+                    lblStatus.setTextFill(Color.RED);
+                }else{
+                    lblStatus.setText("Too low!");
+                    lblStatus.setTextFill(Color.RED);
+                }
+            } catch (Exception e){
+                lblStatus.setText("Enter only numbers.");
+                lblStatus.setTextFill(Color.RED);
+            }
+        }else {
+            lblStatus.setText("");
+            tfGuessedNumber.clear();
+            randomizeMysteryNumber();
+            btnPlay.setText("Guess");
+        }
+    }
+    @FXML
+    void resetScore() {
+        score = 0;
+        lblScore.setText("Score: "+score);
+        lblStatus.setText("");
+        btnReset.setDisable(true);
+        tfGuessedNumber.clear();
+    }
+    @FXML
+    void clearStatusLabelOnTextFieldMouseClick() {
+        lblStatus.setText("");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        randomizeMysteryNumber();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("home.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Guess Number Game");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 }
