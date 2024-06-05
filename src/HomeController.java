60f446ca56643e56264f0ce2e99ec44bbe51271b
TimothyLetsoko <107189293+TimothyLetsoko@users.noreply.github.com>
2024-06-05 04:13:26 +0200

    Update HomeController.java
    Refactored code by adding spaces to make it more readable

--- src/HomeController.java
+++ src/HomeController.java
@@ -3,7 +3,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-//import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
@@ -22,9 +21,11 @@ public class HomeController implements Initializable {
     private Button btnReset;
     @FXML
     private TextField tfGuessedNumber;
+    
     int mysteryNumber;
     int attempts = 3;
     int score;
+    
     void randomizeMysteryNumber(){
         Random random = new Random(System.currentTimeMillis());
         mysteryNumber = random.nextInt(20);
@@ -35,20 +36,24 @@ public class HomeController implements Initializable {
         if(btnPlay.getText().equals("Guess")){
             try{
                 int guessedNumber = Integer.parseInt(tfGuessedNumber.getText());
+                
                 if(mysteryNumber == guessedNumber){
                     lblStatus.setText("You Won!!!");
                     lblStatus.setTextFill(Color.GREEN);
                     btnPlay.setText("Play again");
                     lblScore.setText("Score: "+(++score));
                     btnReset.setDisable(false);
+                    
                 }else if(Math.abs(mysteryNumber-guessedNumber)<5){
                     lblStatus.setText("You are close");
                     lblStatus.setTextFill(Color.ORANGE);
                     lblAttempts.setText("Attempts: "+(--attempts));
+                    
                 }else if((guessedNumber-mysteryNumber)>=5){
                     lblStatus.setText("Too high!");
                     lblStatus.setTextFill(Color.RED);
                     lblAttempts.setText("Attempts: "+(--attempts));
+                    
                 }else{
                     lblStatus.setText("Too low!");
                     lblStatus.setTextFill(Color.RED);
@@ -61,17 +66,22 @@ public class HomeController implements Initializable {
         }else {
             lblStatus.setText("");
             tfGuessedNumber.clear();
+
             randomizeMysteryNumber();
+            
             btnPlay.setText("Guess");
             attempts = 3;
             lblAttempts.setText("Attempts: "+attempts);
         }
+        
         if(attempts==0){
             lblStatus.setText("You lost.");
             attempts=3;
+            
             randomizeMysteryNumber();
         }
     }
+    
     @FXML
     void resetScore() {
         score = 0;
@@ -80,10 +90,12 @@ public class HomeController implements Initializable {
         btnReset.setDisable(true);
         tfGuessedNumber.clear();
     }
+    
     @FXML
     void clearStatusLabelOnTextFieldMouseClick() {
         lblStatus.setText("");
     }
+    
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         randomizeMysteryNumber();
